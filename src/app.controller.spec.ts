18558ff089faa914c73908301a364a030bda6670
nosregor <werdnanosregor@gmail.com>
2025-11-04 11:23:59 +0100

    Initial commit

---
+++ src/app.controller.spec.ts
@@ -0,0 +1,26 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+
+describe("AppController", () => {
+  let appController: AppController;
+  let appService: AppService;
+
+  beforeEach(async () => {
+    const app: TestingModule = await Test.createTestingModule({
+      controllers: [AppController],
+      providers: [AppService],
+    }).compile();
+
+    appService = app.get<AppService>(AppService);
+    appController = app.get<AppController>(AppController);
+  });
+
+  describe("root", () => {
+    it('should return "Hello World!"', () => {
+      const result = "Hello World!";
+      jest.spyOn(appService, "getHello").mockImplementation(() => result);
+      expect(appController.getHello()).toBe(result);
+    });
+  });
+});
